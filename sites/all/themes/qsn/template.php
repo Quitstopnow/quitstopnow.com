b1d42f9a19a772fd3a41c4f1e808a03504b68337
Owen Williams <owenwilliams1@gmail.com>
2015-01-08 13:01:46 +1100

    new files QSN and nav bar styling

--- sites/all/themes/qsn/template.php
+++ sites/all/themes/qsn/template.php
@@ -4,3 +4,9 @@
  * @file
  * template.php
  */
+
+// QSN Preprocess Page
+function qsn_preprocess_page(&$variables) {
+  // unset container class of nav bar
+  unset($variables['navbar_classes_array'][1]);
+}
